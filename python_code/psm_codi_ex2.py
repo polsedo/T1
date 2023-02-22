0a1faa51e016f72574547e79f2afbabae89ff7a1
Pol Sedo <100866973+polsedo@users.noreply.github.com>
2023-02-22 16:31:38 +0100

    exercisi 2 analisis automatic

--- python_code/psm_codi_ex2.py
+++ python_code/psm_codi_ex2.py
@@ -4,7 +4,11 @@ import soundfile as sf
 import sounddevice as sd 
 from numpy.fft import fft
                           
-x_r, fm = sf.read('so_1.wav')
+x_r, fm = sf.read('so_2.wav')
+magspec = plt.magnitude_spectrum(x_r, fm)
+#print(np.mean(magspec.))
+fxx=magspec[1][np.argmax(magspec[0])]
+print(fxx)
 
 T= 2.5  
 fx=4000
@@ -12,13 +16,15 @@ L = int(fm * T)                      # Nombre de mostres del senyal digital
 Tm=1/fm                              # Període de mostratge
 t=Tm*np.arange(L)     
 
-Tx=1/fx                                   # Període del senyal
+Tx=1/fxx                                   # Període del senyal
 Ls=int(fm*5*Tx)  
                 
 print("importado correctamente")
 
 sd.play(x_r, fm) 
 
+
+ 
 plt.figure(0)                             # Nova figura
 plt.plot(t[0:Ls], x_r[0:Ls])                # Representació del senyal en funció del temps
 plt.xlabel('t en segons')                 # Etiqueta eix temporal
