b49f17456f1b9a9907868f58919a41b9851be325
Pol Sedo <100866973+polsedo@users.noreply.github.com>
2023-02-22 15:55:46 +0100

    exercisi 2

---
+++ python_code/psm_codi_ex2.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import soundfile as sf
+import sounddevice as sd 
+from numpy.fft import fft
+                          
+x_r, fm = sf.read('so_1.wav')
+
+T= 2.5  
+fx=4000
+L = int(fm * T)                      # Nombre de mostres del senyal digital
+Tm=1/fm                              # Període de mostratge
+t=Tm*np.arange(L)     
+
+Tx=1/fx                                   # Període del senyal
+Ls=int(fm*5*Tx)  
+                
+print("importado correctamente")
+
+sd.play(x_r, fm) 
+
+plt.figure(0)                             # Nova figura
+plt.plot(t[0:Ls], x_r[0:Ls])                # Representació del senyal en funció del temps
+plt.xlabel('t en segons')                 # Etiqueta eix temporal
+plt.title('5 periodes de la sinusoide')   # Títol del gràfic
+plt.show() 
